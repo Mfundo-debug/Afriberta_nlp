5045dc1b9efb9048606b0410acf78e3985d054d0
Mfundo-debug <diditmfundo@gmail.com>
2023-09-19 06:37:21 +0200

    in progress

---
+++ Afriberta.py
@@ -0,0 +1,8 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, AdamW
+from transformers import Trainer, TrainingArguments
+from datasets import load_dataset, load_metric
+
+#load the dataset ensure dataset is in a language that AfriBerta is pre-trained on
+dataset = load_dataset("swahili_news")
+print(dataset)
