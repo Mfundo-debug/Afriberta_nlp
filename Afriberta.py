130d7e9e0c63e14a41f81dcab377b367b9f2de33
Mfundo-debug <diditmfundo@gmail.com>
2023-09-20 01:55:01 +0200

    will revisit soon

--- Afriberta.py
+++ Afriberta.py
@@ -6,3 +6,6 @@ from datasets import load_dataset, load_metric
 #load the dataset ensure dataset is in a language that AfriBerta is pre-trained on
 dataset = load_dataset("swahili_news")
 print(dataset)
+dataset.shape
+train_data = dataset['train']
+test_data = dataset['test']
